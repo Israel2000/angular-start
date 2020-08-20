6b1c78f40934efd5ec9d2822d24aa43db3290c06
Israel Belete <ibelete2000@gmail.com>
2020-08-19 22:58:26 -0600

    first angular-start repo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-start';
+  title = 'Welcome to Angular-start';
+
+  //  interpolation and template expressions
+  itemImageUrl = '../assets/plant-pic.png';
+
 }
---
+++ src/app/plants.ts
@@ -0,0 +1,10 @@
+import { Plant } from './plant';
+
+export const Plants: Plant[] = [
+    { name: "Aloe Vera" },
+    { name: 'Century Plant' },
+    { name: 'Rose' },
+    { name: 'Cactus' },
+    { name: 'Daisy' },
+    { name: 'Corn' }
+];
